225c9c32bcb04c15ffe0ac31f19d8f9c5a2ceb8a
shabaaz27 <khanshabaaz05@gmail.com>
2022-08-16 10:37:39 +0530

    Handling Post Requset Basics with middleware

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json()) //middleware
+
 // app.get('/',(req,res)=>{
 //     res.status(200).json({message:"Hello from server",app:"Natorus"})
 // })
@@ -26,6 +28,21 @@ res.status(200).json({
 })
 
 })
+
+app.post('/api/v1/tours',(req,res)=>{
+    // console.log(req.body)
+    const newId = tours[tours.length-1].id+1
+    const newTour = Object.assign({id:newId},req.body)
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err=>{
+        res.status(201).json({
+           status:"success",
+            code:201,
+            data:{tour:newTour}
+        })
+    })
+})
+
 
 
 
