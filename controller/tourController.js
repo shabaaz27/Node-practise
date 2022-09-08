3de967517fbb207fbe16a181e085a034ddc5cb4e
shabaaz27 <khanshabaaz05@gmail.com>
2022-09-08 13:08:34 +0530

    check body before post request

--- controller/tourController.js
+++ controller/tourController.js
@@ -14,6 +14,16 @@ exports.checkId=(req,res,next,val)=>{
       next()
 }
 
+exports.checkBody = (req,res,next)=>{
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).json({
+          status: 400,
+          message: 'Missing name or price',
+        });
+      }
+      next()
+}
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -42,8 +52,9 @@ exports.getTour = (req, res) => {
   
 };
 
+
 exports.createTour = (req, res) => {
-  // console.log(req.body)
+  console.log(req.body)
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
