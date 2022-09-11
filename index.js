edf47f192a9eb5fb3b27fecd60b7421ac573f406
shabaaz27 <khanshabaaz05@gmail.com>
2022-09-11 18:05:02 +0530

    packages

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-
 const express = require('express');
 const morgan = require('morgan');
 const router = require('./routes/tourRoutes');
+
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-console.log(process.env.NODE_ENV );
+console.log(process.env.NODE_ENV);
 //1.middlewares
-if(process.env.NODE_ENV === 'development'){
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
-app.use(express.json()); 
-app.use(express.static(`${__dirname}/public`))
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 //create you own middleware
-app.use((req,res,next)=>{
-  console.log('from the middle ware')
-  next()
-})
-
+app.use((req, res, next) => {
+  console.log('from the middle ware');
+  next();
+});
 
 //old way
 // app.get('/api/v1/tours', getAllTours);
@@ -35,10 +34,9 @@ app.use((req,res,next)=>{
 
 //3.route
 // mounting routers
-app.use('/api/v1/tours',router)
-app.use('/api/v1/users',userRouter)
-
+app.use('/api/v1/tours', router);
+app.use('/api/v1/users', userRouter);
 
 //4.Start Sever
 
-module.exports = app
+module.exports = app;
