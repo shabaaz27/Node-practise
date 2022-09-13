3e8d5461e18021e73e725b7a73b2390d4f634a25
shabaaz27 <khanshabaaz05@gmail.com>
2022-09-13 18:44:26 +0530

    connect localDb testdata using schema,model,save

--- server.js
+++ server.js
@@ -17,9 +17,42 @@ mongoose.connect(process.env.DATABASE_LOCAL,{
   useFindAndModify:false
 }).then(()=> console.log("DataBase connection successful"))
 
+
+const tourSchema = new mongoose.Schema({
+  name:{
+    type:String,
+    required:[true,'A tour must have a name'],
+    unique:true
+  },
+  rating:{
+    type:Number,
+    default:4.5
+  },
+  price:{
+    type:Number,
+    required:[true,'A tour must have a price']
+  }
+})
+
+const Tour = mongoose.model('Tour',tourSchema)
+
+const testTour = new Tour({
+  name:'The Forest Hiker',
+  rating:4.7,
+  price:597
+});
+
+const secondTour = new Tour({
+  name:'The Sky Diver',
+  rating:4.9,
+  price:1000
+});
+
+testTour.save().then((doc)=>console.log(doc)).catch(err=>console.log(err.message,'🤦‍♂️'))
+
+
+
 const port = process.env.port || 3000;
-
-
 app.listen(port, () => {
   console.log('Listening port ', { port });
 });
