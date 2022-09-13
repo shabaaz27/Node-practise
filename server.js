ab6c275d35845c17a7a085e5b0252c827a243c68
shabaaz27 <khanshabaaz05@gmail.com>
2022-09-13 17:53:48 +0530

    create connection using mongoose with mongoDB

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const dotenv = require('dotenv')
-dotenv.config({path:'./config.env'})
-
-
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./index');
 
 
 
+
+dotenv.config({path:'./config.env'})
+
+
+const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATA_PASSWORD);
+
+mongoose.connect(process.env.DATABASE_LOCAL,{
+  useNewUrlParser:true,
+  useCreateIndex:true,
+  useUnifiedTopology:true,
+  useFindAndModify:false
+}).then(()=> console.log("DataBase connection successful"))
 
 const port = process.env.port || 3000;
 
