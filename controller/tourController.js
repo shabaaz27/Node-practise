e368900c00940739e64278aa3d9a02fbfecf901d
shabaaz27 <khanshabaaz05@gmail.com>
2022-09-16 11:19:41 +0530

    Api Aliasing

--- controller/tourController.js
+++ controller/tourController.js
@@ -25,6 +25,21 @@ const Tour = require('../modals/tourModal')
 //       next()
 // }
 
+  exports.aliasTopTours= async (req,res,next)=>{
+      req.query.limit = '5';
+      req.query.sort = '-ratingAverage,price';
+      req.query.fields ='name,price,ratingAverage,summary,difficulty';
+      next()
+  }
+
+  exports.highPrice =async (req,res,next)=>{
+    req.query.limit = '5';
+    req.query.sort = 'ratingAverage,-price';
+    req.query.fields ='name,price,ratingAverage,summary,difficulty';
+    next()
+}
+
+
 exports.getAllTours = async (req, res) => {
 
   try{ 
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const { getAllTours, createTour, getTour, updateTour, deleteTour } = require('../controller/tourController')
+const { getAllTours, createTour, getTour, updateTour, deleteTour,aliasTopTours,highPrice } = require('../controller/tourController')
 
 const router = express.Router() //middleware
 
 
 
- 
-
+router.route('/top-5-cheap').get(aliasTopTours,getAllTours)
+router.route('/top-5-luxury').get(highPrice,getAllTours)
 router.route('/').get(getAllTours).post(createTour)
 router.route('/:id').get(getTour).put(updateTour).delete(deleteTour)
 
