540a2ec232e7a55611a271f522fbfdf0c4c0af8b
shabaaz27 <khanshabaaz05@gmail.com>
2022-09-25 21:25:38 +0530

    user create functionality added

---
+++ modals/userModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+//name, email, photo, password,passwordConfirm
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: [true, 'A user must have a name'],
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: true,
+    lowercase: true,
+    required: [true, `Please provide your email`],
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const userController = require('../controller/userController')
+const authController = require('../controller/authController')
 
 const userRouter = express.Router()
+
+userRouter.post('/signup',authController.signup)
 
 userRouter.route('/').get(userController.getAllUsers).post(userController.createUser)
 userRouter.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser)
