5d64711914d844ee8b1676dcd03bd1f6dc5c012c
shabaaz27 <khanshabaaz05@gmail.com>
2022-09-25 15:31:14 +0530

    Errors during devlopment and production

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,11 +1,39 @@
-module.exports = (err,req,res,next)=>{
-    console.log(err.stack)
-    err.statusCode = err.statusCode || 500
-    err.status = err.status || 'error';
-  
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    statusCode: err.statusCode,
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //Operational, trusted error : send message to client
+  if (err.isOperational) {
     res.status(err.statusCode).json({
-      statusCode:err.statusCode,
-      status:err.status,
-      message:err.message
-    })
+      status: err.statusCode,
+      message: err.message,
+    });
+    //Programming or other unknown error : don't leak error details
+  } else {
+    //1) Log error
+    console.error('ERROR 💥', err);
+    //2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'something went very wrong',
+    });
   }
+};
+
+module.exports = (err, req, res, next) => {
+  console.log(err.stack);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
+};
