532b5e582c50680197d8d993ed934297febd2e2a
shabaaz27 <khanshabaaz05@gmail.com>
2022-09-26 22:53:59 +0530

    compare passwrd and password confirm

--- modals/userModel.js
+++ modals/userModel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-//name, email, photo, password,passwordConfirm
+const bcrypt = require('bcryptjs')
 
+//name, email, photo, password,passwordConfirm
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -27,8 +28,30 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate:{
+        //this only work on CREATE AND  SAVE!!!
+        validator:function(el){
+            return el === this.password; //validation
+        }, 
+        message:'Password and confrim password should be same'    
+    }
   },
 });
+
+
+userSchema.pre('save',async function(next){
+    // Only run if password was actually modified
+    if(!this.isModified('password')) return next()
+
+    // hash password with cost 12
+    this.password = await bcrypt.hash(this.password,12);
+
+    //delete passwordConfirm field
+    this.passwordConfirm = undefined;
+
+    console.log(this.password)
+    next()
+})
 
 const User = mongoose.model('User', userSchema);
 
